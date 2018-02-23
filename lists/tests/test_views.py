6967c4be0cc8122fc988af08407dd2a98bfb3863
Xander Madsen <xander.madsen@gmail.com>
2018-02-23 08:45:39 -0500

    use new form in home_page, simplify tests. NB breaks stuff.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -106,3 +106,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_home_page_uses_item_form(self):
+        response = self.client.get('/')
+        self.assertIsInstance(response.context['form'], ItemForm)
